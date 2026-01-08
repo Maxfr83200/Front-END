3a49e44bc83878aa17763f8a94e679054b54041e
Maxfr83200 <maxfr83200@gmail.com>
2026-01-08 18:44:01 +0100

    Add navigation from paiement to accueil on click
    Implemented startCommand in paiementF to navigate to accueil.fxml when the AnchorPane is clicked. Updated paiement.fxml to set fx:controller and onMouseClicked handler, and changed the displayed order number.

--- src/main/java/com/paiementF.java
+++ src/main/java/com/paiementF.java
@@ -1,4 +1,36 @@
 package com;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class paiementF {
+
+    private Boolean isFrench = true;
+
+
+    @FXML
+    public void startCommand(MouseEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/accueil.fxml"));
+        try {
+            Parent root = loader.load();
+            accueilF mainCtrl = loader.getController();
+            
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
